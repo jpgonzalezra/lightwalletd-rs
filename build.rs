c166bece9d62b440041b2de9f26959f50f2836f4
jpgonzalezra <jpgonzalezra@gmail.com>
2026-06-17 12:12:33 -0300

    build: generate gRPC client code

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
-        .build_client(false)
+        .build_client(true)
         .compile_protos(&["proto/service.proto", "proto/darkside.proto"], &["proto"])?;
     Ok(())
 }
